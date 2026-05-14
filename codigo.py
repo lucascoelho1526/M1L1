b098149d830a6f5d83f24ef31adf1165a30abc6b
lucascoelho1526 <sscoelho.lucas@gmail.com>
2026-05-14 20:19:04 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,11 +1,41 @@
-import random
-senha = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
 
-comprimento = int(input("digite sua senha: "))
-armazenador = ""
+intents = discord.Intents.
+default()
+intents.menssage_content = True
+
+bot = commands.bot
+(command_prefix = '$' , intents = intents)
+
+@bot.event
+async def on_ready():
+    print (f'fizemo login como {bot.user}')
+
+@bot.event
+async def on_messsage(message):
+    if message.autor == bot.user :
+        return
+    
+    await bot.process_commands(message)
+
+async def hello(ctx) :
+    await ctx.send('hi')
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("\U0001f642")
+
+bot.command()
+async def senha(ctx):
+    await ctx.send('sua senha :' + gen_pass(10))
+
+bot.command()
+async def test(ctx , arg):
+    await ctx.send(arg)
+
+bot.run('TOKEN')
+    
 
 
-for i in range(comprimento):
-    armazenador += random.choice(senha)
-
-print(armazenador)
